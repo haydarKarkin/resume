e07d72d91423b4c9913768287a93c53a17bd1f4a
Haydar Karkin <haydar.karkin@gmail.com>
2019-09-05 20:22:27 +0300

    Project init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Resume msg="Coming Soon"/>
+  </div>
+</template>
+
+<script>
+import Resume from './components/Resume.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Resume
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 50px;
+}
+</style>
