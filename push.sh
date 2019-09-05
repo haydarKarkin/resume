6ddff59ee1d39d8db51952dfd417155e8c310e55
Haydar Karkin <haydar.karkin@gmail.com>
2019-09-06 01:14:08 +0300

    Update push script

--- push.sh
+++ push.sh
@@ -2,23 +2,36 @@
 
 set -ev
 
+USERNAME="haydarKarkin"
+PROJECT_NAME="haydarkarkin.github.io"
+REPO="${USERNAME}/${PROJECT_NAME}"
+
 setup_git() {
   git config credential.helper "store --file=.git/credentials"
   echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-
-  git pull origin master
 }
 
-commit_website_files() {
-  git add dist
+setup_other_repo() {
+  echo "${REPO} cloning..."
+  git clone https://${GH_TOKEN}@github.com/${REPO}.git
+  echo "${REPO} cloned"
+}
+
+commit_static_files_and_push() {
+  echo "Moving generated static files..."
+  mv dist/* ${PROJECT_NAME}/
+  
+  echo "Commiting changes..."
+  cd ${PROJECT_NAME}
+  git add .
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
-
-upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/haydarKarkin/haydarkarkin.github.io.git > /dev/null 2>&1
+  echo "Commited change"
+  
+  echo "Pushing changes..."
   git push origin master
+  echo "Pushed changes"
 }
 
 setup_git
-commit_website_files
-upload_files
+setup_other_repo
+commit_static_files_and_push
