7b304f980dd7319ad1b0eb455b23aff3cbbe8904
Haydar Karkin <haydar.karkin@gmail.com>
2019-09-06 00:55:43 +0300

    Update push script

--- push.sh
+++ push.sh
@@ -5,6 +5,9 @@ set -ev
 setup_git() {
   git config credential.helper "store --file=.git/credentials"
   echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
+  git remote add origin https://${GH_TOKEN}@github.com/haydarKarkin/haydarkarkin.github.io.git > /dev/null 2>&1
+  git pull origin master
 }
 
 commit_website_files() {
@@ -13,9 +16,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-site https://${GH_TOKEN}@github.com/haydarKarkin/haydarkarkin.github.io.git > /dev/null 2>&1
-  git pull origin-site master
-  git push origin-site master
+  git push origin master
 }
 
 setup_git
