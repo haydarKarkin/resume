cd97229391ccc1c409e265ac05bc6c3402ef8f70
Haydar Karkin <haydar.karkin@gmail.com>
2019-09-05 22:49:56 +0300

    Update user name and mail for git commit

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  git config --global user.email "haydar.karkin@gmail.com"
+  git config --global user.name "Haydar Karkin"
 }
 
 commit_website_files() {
