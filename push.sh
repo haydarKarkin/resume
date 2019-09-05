11db810146ba57d5f2c09efc313c34c87a5ad564
Haydar Karkin <haydar.karkin@gmail.com>
2019-09-06 00:10:00 +0300

    Update travis&push.sh script

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "haydar.karkin@gmail.com"
-  git config --global user.name "Haydar Karkin"
+  git config credential.helper "store --file=.git/credentials"
+  echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 }
 
 commit_website_files() {
